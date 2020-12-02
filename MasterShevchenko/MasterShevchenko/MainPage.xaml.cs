9ea7aa72a70ebb3a6787c99c60f3d3429f9ecef0
Opilane TTHK <opilane@tthk.ee>
2020-12-02 11:21:14 +0200

    Add project files.

---
+++ MasterShevchenko/MasterShevchenko/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterShevchenko
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            profileImage.Source = ImageSource.FromFile("image.jpg");
+            aboutList.ItemsSource = GetMenuList();
+            var homePage = typeof(Views.AboutPage);
+            Detail = new NavigationPage((Page)Activator.CreateInstance(homePage));
+            IsPresented = false;
+        }
+
+        public List<MasterMenuItems> GetMenuList()
+        {
+            var list = new List<MasterMenuItems>();
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minust",
+                Detail = "Lühike info",
+                ImagePath = "about.png",
+                TargetPage = typeof(Views.AboutPage)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minu kogemus",
+                Detail = "Natue rohkem minu kogemisest",
+                ImagePath = "list.png",
+                TargetPage = typeof(Views.ExperiencePage)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minu oskused",
+                Detail = "Natue rohkem minu oskustest",
+                ImagePath = "skill.png",
+                TargetPage = typeof(Views.SkillsPage)
+            });
+            list.Add(new MasterMenuItems()
+            {
+                Text = "Minu võidud",
+                Detail = "Ma olen uhke!!!",
+                ImagePath = "awards.png",
+                TargetPage = typeof(Views.AchievemnetsPage)
+            });
+            return list;
+        }
+        private void aboutList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedMenuItem = (MasterMenuItems)e.SelectedItem;
+            Type selectedPage = selectedMenuItem.TargetPage;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(selectedPage));
+            IsPresented = false;
+        }
+
+    }
+}
+ 
